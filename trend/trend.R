7e34e4205c4757066ce6237214ef4478b9ba6879
Edward Ionides <ionides@umich.edu>
2016-01-12 10:56:04 -0500

    class 2 v2

--- trend/trend.R
+++ trend/trend.R
@@ -16,9 +16,13 @@ lm_fit <- lm(Annual~Year+I(Year^2),data=global_temp)
 summary(lm_fit)
 
 ## ----glob_temp_lm_plot---------------------------------------------------
-yr <- 1880:2020
+yr <- 1880:2026
 Z <- cbind(1,yr,yr^2)
 beta <- coef(lm_fit)
-plot(Annual~Year,data=global_temp,ty="l",xlim=range(yr))
-lines(x=yr,y=Z%*%beta)
+prediction <- Z%*%beta
+plot(Annual~Year,data=global_temp,ty="l",xlim=range(yr),ylim=range(c(global_temp$Annual,prediction),na.rm=TRUE),lty="dashed")
+lines(x=yr,y=prediction,col="red")
+
+## ----acf_global_temp-----------------------------------------------------
+acf(resid(lm_fit))
 
